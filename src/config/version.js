7bea40e6b13ac14ab8e6c76e9282bd3641db4fe4
johnpark236 <johnpark236@gmail.com>
2026-09-02 03:24:20 +0900

    chore: prepare TodayPick vc74 internal release

--- src/config/version.js
+++ src/config/version.js
@@ -1,7 +1,7 @@
 /**
  * TodayPick Version Information (Single Source of Truth)
  */
-export const APP_DISPLAY_VERSION = 'v0.73';
-export const ANDROID_VERSION_CODE = 73;
+export const APP_DISPLAY_VERSION = 'v0.74';
+export const ANDROID_VERSION_CODE = 74;
 export const APP_VERSION_NAME = '1.0.0';
 
